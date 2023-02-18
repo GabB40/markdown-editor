f1947ad540d48a5552a66aba2dea4029ffd94205
GabB40 <gabriel.beras@gmail.com>
2023-02-18 21:43:31 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SimplemdeComponent, SimplemdeOptions } from 'ngx-simplemde';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'markdown-editor';
+export class AppComponent implements OnInit {
+  @ViewChild('simplemde', { static: true }) private readonly simplemde!: SimplemdeComponent;
+  simplemdeContent = `# test heading
+  ## test heading 2
+  Un peu de texte avec un mot en *gras* et un en _italique_`;
+  options: SimplemdeOptions = {
+    /* toolbarTips: false,  */
+    status: false
+  }
+
+  ngOnInit(): void {
+  }
+
 }
